788d98e525b65f4ad9280e0c664d59dd83faf93d
Erick F <erickflores785@gmail.com>
2019-03-31 16:27:38 -0500

    Added css files and styling

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // app.use('/', (req, res, next) => {
 //     console.log(`Received ${req.method} request on path ${req.url}`);
