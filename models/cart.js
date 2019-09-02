c9cc2b892f900246359df961e5a570176464421b
Erick F <erickflores785@gmail.com>
2019-09-01 19:04:42 -0500

    delete item

--- models/cart.js
+++ models/cart.js
@@ -38,4 +38,22 @@ module.exports = class Cart {
       });
     });
   }
+  static deleteProduct (id, productPrice) {
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        return;
+      }
+      // @ts-ignore
+      const updatedCart = {...JSON.parse(fileContent)};
+      const product = updatedCart.products.find(prod => prod.id === id);
+      const productQty = product.qty;
+      updatedCart.proudcts = updatedCart.products.filter(
+        prod => prod.id !== id
+      );
+      updatedCart.totalPrice = updatedCart.totalPrice - product.price * productQty;
+      fs.writeFile(p, JSON.stringify(updatedCart), err => {
+        console.log(err);
+      });
+    });
+  }
 };
--- routes/admin.js
+++ routes/admin.js
@@ -5,16 +5,17 @@ const adminController = require("../controllers/admin");
 
 const router = express.Router();
 
-// GET
+
 router.get("/add-product", adminController.getAddProduct);
 
 router.get("/products", adminController.getProducts);
 
-// POST
 router.post("/add-product", adminController.postAddProduct);
 
 router.get("/edit-product/:productId", adminController.getEditProduct);
 
 router.post("/edit-product", adminController.postEditProduct);
+
+router.post("/delete-product", adminController.postDeleteProduct);
 
 module.exports = router;
