35701890eaba539d3ad7b11d97ce93b134a1e8fe
Erick F <erickflores785@gmail.com>
2019-12-30 11:40:23 -0600

    signup views, controllers, and routes

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,31 +1,39 @@
-const User = require("../models/user");
+const User = require('../models/user');
 
 exports.getLogin = (req, res, next) => {
-  res.render("auth/login", {
-    path: "/login",
-    pageTitle: "Login",
+  res.render('auth/login', {
+    path: '/login',
+    pageTitle: 'Login',
     isAuthenticated: false
   });
 };
-// Stores user in session: session contains 'login' value. This session will then be shared to other middleware that require rendering when interacting with the user.
+
+exports.getSignup = (req, res, next) => {
+  res.render('auth/signup', {
+    path: '/signup',
+    pageTitle: 'Signup',
+    isAuthenticated: false
+  });
+};
+
 exports.postLogin = (req, res, next) => {
-  User.findById("5d82bf38b820a20de55a472e")
+  User.findById('5bab316ce0a7c75f783cb8a8')
     .then(user => {
       req.session.isLoggedIn = true;
       req.session.user = user;
       req.session.save(err => {
         console.log(err);
-        res.redirect("/");
-      })
+        res.redirect('/');
+      });
     })
-    .catch(err => {
-      console.log(err);
-    });
+    .catch(err => console.log(err));
 };
+
+exports.postSignup = (req, res, next) => {};
 
 exports.postLogout = (req, res, next) => {
   req.session.destroy(err => {
     console.log(err);
-    res.redirect("/");
+    res.redirect('/');
   });
 };
