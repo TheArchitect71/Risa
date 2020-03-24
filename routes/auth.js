6d0dc20e536f4cf349cbc1deb77138a6655ec4f3
Erick F <erickflores785@gmail.com>
2020-03-24 00:02:36 -0500

    password validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { check } = require("express-validator");
+const { check, body } = require("express-validator");
 
 const authController = require("../controllers/auth");
 
@@ -13,9 +13,29 @@ router.post("/login", authController.postLogin);
 
 router.post(
   "/signup",
-  check("email")
-    .isEmail()
-    .withMessage("Please Enter a Valid Email"),
+  [
+    check("email")
+      .isEmail()
+      .withMessage("Please Enter a Valid Email")
+      .custom((value, { req }) => {
+        if (value === "test@email.com") {
+          throw new Error("This is email is forbidden.");
+        }
+        return true;
+      }),
+    /*The second parameter will be the .message for errors */
+    body("password", "Please input a password at least 8 characters long")
+      /*Password should be at least 8 characters long in production
+    and require uppercase, lowercase, number, and symbol*/
+      .isLength({ min: 6, max: 20 })
+      .isAlphanumeric(),
+      body('confirmpassword').custom((value, { req }) => {
+        if (value !== req.body.passowrd) {
+          throw new Error('Passwords do not match');
+        }
+        return true;
+      })
+  ],
   authController.postSignup
 );
 
